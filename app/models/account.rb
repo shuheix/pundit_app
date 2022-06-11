8a5e1d6859a234ed3a80143871248c0d73063135
onos <ono.shuhei210@gmail.com>
2022-06-12 07:43:12 +0900

    add: comment

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
   has_many :notes  # 中間テーブルを経由しない
   has_many :account_notes
-  has_many :collaboration_notes, through: :account_notes, source: :note # 命名を指定
+  has_many :collaboration_notes, through: :account_notes, source: :note # 中間テーブルを経由する
   has_many :company_account
   has_many :company, through: :company_account
   has_many :account_groups
