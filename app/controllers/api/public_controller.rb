ecb91ab75a045b5768687afbfadabfd5acb066bc
shuheix <ono.shuhei210@gmail.com>
2022-06-17 18:17:19 +0900

    delete

--- app/controllers/api/public_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module Api
-  class PublicController < ActionController::API
-    def public
-      render json: { message: 'You don\'t need to be authenticated to call this' }
-    end
-  end
-end
